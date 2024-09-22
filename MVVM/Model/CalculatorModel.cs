7aae49f818452d5fc26009f6e0e42ea3646ee775
JaeBeen Lee <ljb6797@naver.com>
2024-09-22 17:26:51 +0900

    Fix "숫자가 매우 클 때 생기는 오류를 지수 표기법으로 표현해 수정"

--- MVVM/Model/CalculatorModel.cs
+++ MVVM/Model/CalculatorModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Calculator.MVVM.Model
@@ -10,9 +11,16 @@ namespace Calculator.MVVM.Model
     {
         public object Calculation(string expression)
         {
-            var dataTable = new System.Data.DataTable();
+            var dataTable = new DataTable();
             var result = Convert.ToDouble(dataTable.Compute(expression, string.Empty));
 
+            // 지수 표기법으로 변환 (숫자가 매우 클 때만)
+            if (result > 999999999 || result < -999999999)
+            {
+                return result.ToString("E");
+            }
+
+            // 정수 결과 처리
             return result % 1 == 0 ? (int)result : result;
         }
     }
